ba00de9afa00699930e9425af3a8610d38ce1188
Jazzriv <voidgrape@gmail.com>
2015-12-27 23:35:44 +0100

    Create menu_scripts.js

---
+++ scripts/menu_scripts.js
@@ -0,0 +1,9 @@
+function showhide_login(){
+	if(document.getElementById('loginbox').style.visibility == "visible")
+		document.getElementById('loginbox').style.visibility='hidden';
+
+	
+	else
+		document.getElementById('loginbox').style.visibility='visible';
+
+}
