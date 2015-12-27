71347d1802303a92003b24a9562249b4e7f33929
Jazzriv <voidgrape@gmail.com>
2015-12-27 23:34:07 +0100

    Create database.php

---
+++ PHP/database.php
@@ -0,0 +1,22 @@
+<?php
+
+//Connexion à la BDD movies
+/*try
+{
+    $videos = new PDO('mysql:host=localhost;dbname=../databases/movies-v2;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}*/
+
+//Connexion à la BDD users
+try
+{
+    $users = new PDO('mysql:host=localhost;dbname=users;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+?>
