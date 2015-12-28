44250d54d8341ea83ba264f7b7150005e58be8b6
Jazzriv <voidgrape@gmail.com>
2015-12-28 01:58:48 +0100

    Create sign_form.php

---
+++ PHP/sign_form.php
@@ -0,0 +1,15 @@
+<div>
+	<form method="post" action="signin.php">
+		<div class="signup_form">
+			<label for="username">Nom d'utilisateur :</label> <input type="text" name="username" id="username" placeholder="Nom d'utilisateur"/>
+		</div>
+		<div class="signup_form">
+			<label for="password">Mot de passe :</label> <input type="password" name="password" id="password"/>
+		</div>
+		<div class="signup_form">
+			<label for="email">E-mail :</label> <input type="email" id="email" name="email" placeholder="E-mail"/>
+		</div>
+		<div class="signup_form">
+			<input type="submit" value="S'enregistrer">
+	</form>
+</div>
