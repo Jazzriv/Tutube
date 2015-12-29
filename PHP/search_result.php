4944c4400d4158aeaee950243b2af428c2a0d4ec
Jazzriv <voidgrape@gmail.com>
2015-12-29 15:53:45 +0100

    Create search_result.php

---
+++ PHP/search_result.php
@@ -0,0 +1,15 @@
+<div class="viddiv">
+	<div class="vidthumbnail">
+		<a href="view.php"><img src="$data['thumbnail']"></a>
+	</div>
+	<div class="vidinfo">
+		<h5><a href="view.php">$data['title']</a></h5>
+		<br/>
+		<h6>Synopsis : </h6>
+		Decription
+	</div>
+	<div class="vidinfo">
+		<h6>Auteur : </h6>
+		$data['creator']
+	</div>
+</div>
